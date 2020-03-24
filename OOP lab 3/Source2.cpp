2b72bab596cf51744634e8d0c29b2574996a3c8f
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2020-03-24 19:06:32 +0400

    j

---
+++ OOP lab 3/Source2.cpp
@@ -0,0 +1,38 @@
+//class Animal
+//{
+//public:
+//	virtual void Move() = 0;
+//};
+//
+//class cat : public Animal
+//{
+//public:
+//	void Move()
+//	{
+//
+//	}
+//};
+//
+//class dog : public Animal
+//{
+//public:
+//	void Move()
+//	{
+//
+//	}
+//};
+//
+//int main()
+//{
+//	Animal* animals[10];
+//
+//	animals[0] = new cat;
+//	animals[1] = new dog;
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		animals[i]->Move();
+//	}
+//
+//	return 0;
+//}
--- mdpl/lab.cpp
+++ mdpl/lab.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 using namespace std;
 
+char regt[16][3] = { "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh", "ax", "cx", "dx", "bx", "sp", "bp", "si", "di" };
+
 struct Mem_t
 {
 	string name;
@@ -11,21 +13,23 @@ struct Mem_t
 	char rm;
 };
 
+
 struct Operation
 {
 	string opcode;
 	bool d;
 	bool w;
-	char mod;
-	char reg;
-	char rm;
-	char disp_l;
-	char disp_h;
+	string mod;
+	string reg;
+	string rm;
+	string disp_l;
+	string disp_h;
 };
 
-char t[4] = "000";
-void i2b(char x)
+
+string i2b(char x)
 {
+	string t = "000";
 	char m = 1;
 	for (size_t i = 0; i < 3; i++)
 	{
@@ -33,19 +37,28 @@ void i2b(char x)
 		else t[2 - i] = '0';
 		m = m << 1;
 	}
+	return t;
 }
 
-void reg_reg(Operation *op, vector <pair<int, int>> positions)
+void reg_reg(Operation *op, vector<string> opname)
 {
 	op->d = 1;
-	op->w = positions[0].second >> 3;
+	char reg, rm;
+	for (size_t i = 0; i < 16; i++)
+	{
+		if (regt[i] == opname[0])
+			reg = i;
+		else if (regt[i] == opname[1])
+			rm = i;
+	}
 
-	op->mod = 3;
-	op->reg = positions[0].second - 8;
-	op->rm = positions[1].second - 8;
+	op->w = reg >> 3;
+	op->mod = "11";
+	op->reg = i2b(reg);
+	op->rm = i2b(rm);
 }
 
-void reg_mem(Operation* op, vector <pair<int, int>> positions)
+void reg_mem()
 {
 
 }
@@ -57,14 +70,16 @@ void reg_memDisp()
 
 int main()
 {
-	char regt[12][3] = { "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh", "ax", "cx", "dx", "bx"};
+	
 	Mem_t memt[6] = { {"[bp]", 0, 6}, {"[bx]", 0, 7}, {"[bp + disp_l]", 1, 6}, {"[bx + disp_l]", 1, 7}, {"[bp + disp_h:disp_l]", 2, 6}, {"[bx + disp_h:disp_l]", 2, 7} };
-	string name = "add";
-	int count = 0;
 
+	string name = "add";
 	string command;
 	getline(cin, command);
 	Operation op;
+
+	bool operand[2]; // 1 - reg, 0 - mem
+	vector<string> opname;
 
 	for (int i = 0; i < command.size(); i++)
 	{
@@ -73,6 +88,46 @@ int main()
 		name[i] = command[i];
 	}
 	
+	for (int i = 0, j = 0; i < command.size(); i++)
+	{
+		if (command[i] == ' ')
+		{
+			i++;
+			if (command[i] == '[')
+			{
+				operand[j++] = false;
+ 				int endName = i;
+				while (command[endName] != ',')
+					endName++;
+
+				opname.push_back(command.substr(i, endName - i));
+				i = endName;
+			}
+			else
+			{
+				operand[j++] = true;
+				opname.push_back(command.substr(i, 2));
+			}
+		}
+		if (command[i] == ',')
+		{
+			i++;
+			if (command[i] == ' ')
+				i++;
+			if (command[i] == '[')
+			{
+				operand[j++] = false;
+				opname.push_back(command.substr(i, command.size() - i));
+			}
+			else
+			{
+				operand[j++] = true;
+				opname.push_back(command.substr(i, 2));
+			}
+			break;
+		}
+	}
+
 	if (name == "add")
 		op.opcode = "000000";
 	else if (name == "and")
@@ -80,8 +135,28 @@ int main()
 	else
 		op.opcode = "001100";
 
-	vector <pair<int, int>> positions;
-	for (size_t j = 0; j < 12; j++)
+	if (operand[0] && operand[1])
+	{
+		reg_reg(&op, opname);
+	}
+	else if (!operand[0] && operand[1])
+	{
+		if (opname[0].size() > 4)
+			reg_memDisp();
+		else
+			reg_mem();
+	}
+	else if (operand[0] && !operand[1])
+	{
+		if (opname[1].size() > 4)
+			reg_memDisp();
+		else
+			reg_mem();
+	}
+
+
+	//vector <pair<int, int>> positions;
+	/*for (size_t j = 0; j < 12; j++)
 	{
 		int t = command.find(regt[j]);
 		if (t != -1)
@@ -89,12 +164,12 @@ int main()
 			count++;
 			positions.push_back({ t, j });
 		}
-	}
+	}*/
 
-	sort(positions.begin(), positions.end());
+	//sort(positions.begin(), positions.end());
 
-	if (count == 2)
-		reg_reg(&op, positions);
+	//if (count == 2)
+	//	reg_reg(&op, positions);
 
 	return 0;
 }
