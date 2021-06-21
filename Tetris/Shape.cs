211ae77512b0d4de6b7f70f92c14e7acda4d6121
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2021-06-21 21:09:44 +0400

    Tetris Initial Commit

---
+++ Tetris/Shape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Shape
+    {
+        private bool[,] figureData;
+        private Color color;
+        private int currentFigureState;
+        private int figureNumber;
+
+        public Shape(Random rand, List<List<bool[,]>> figures, List<Color> possibleColors)
+        {
+            currentFigureState = 0;
+            int randIndex = rand.Next(figures.Count);
+            figureData = figures[randIndex][currentFigureState];
+            figureNumber = randIndex;
+
+            randIndex = rand.Next(possibleColors.Count);
+            color = possibleColors[randIndex];
+        }
+
+        public void Rotate(List<bool[,]> figureStates)
+        {
+            currentFigureState = currentFigureState == figureStates.Count - 1 ? 0 : currentFigureState + 1;
+            figureData = figureStates[currentFigureState];
+        }
+    }
+}
