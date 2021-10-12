731286e04c63573c2e709f00278413ceb3ce93a0
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2021-10-12 12:40:34 +0400

    sr lab 1

---
+++ Serialization/Output.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public class Output
+    {
+        public decimal SumResult { get; set; }
+        public int MulResult { get; set; }
+        public decimal[] SortedInputs { get; set; }
+
+        public void CalcSum(decimal[] Sums, int K)
+        {
+            SumResult = K * Sums.Sum();
+        }
+
+        public void CalcMul(int[] Muls)
+        {
+            MulResult = Muls.Aggregate(1, (a, b) => a * b);
+        }
+
+        public void SortInputs(int[] Muls, decimal[] Sums)
+        {
+            SortedInputs = new decimal[Muls.Length + Sums.Length];
+            decimal[] MulsDecimalArr = Array.ConvertAll(Muls, x => (decimal) x);
+
+            Array.Copy(Sums, SortedInputs, Sums.Length);
+            Array.Copy(MulsDecimalArr, 0, SortedInputs, Sums.Length, Muls.Length);
+            Array.Sort(SortedInputs);
+        }
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Serialization
+{
+    class Program
+    {
+        private Output GetOutputObject(Input input)
+        {
+            Output output = new Output();
+            output.CalcSum(input.Sums, input.K);
+            output.CalcMul(input.Muls);
+            output.SortInputs(input.Muls, input.Sums);
+            return output;
+        }
+
+        public string GetSerializedOutputJSON(string jsonInput)
+        {
+            Input input = JsonSerializer.Deserialize<Input>(jsonInput);
+            Output output = GetOutputObject(input);
+            string jsonOutput = JsonSerializer.Serialize(output);
+            return jsonOutput;
+        }
+
+        public Input DeserializeXML(string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Input));
+            StreamReader reader = new StreamReader(filePath);
+            Input input = (Input)serializer.Deserialize(reader);
+            reader.Close();
+            return input;
+        }
+
+        public static void SerializeToXML(Output output, string xmlFilePath)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Output));
+            StreamWriter writer = new StreamWriter(xmlFilePath);
+            xmlSerializer.Serialize(writer, output);
+            writer.Close();
+        }
+
+        public void SerializeOutputXML(string inputXmlFile, string outputXmlFile)
+        {
+            Input input = DeserializeXML(inputXmlFile);
+            Output output = GetOutputObject(input);
+            SerializeToXML(output, outputXmlFile);
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            string serializationType = Console.ReadLine();
+
+            if (serializationType.Equals("json"))
+            {
+                string jsonInput = Console.ReadLine();
+                Console.WriteLine(program.GetSerializedOutputJSON(jsonInput));
+            } 
+            else if (serializationType.Equals("xml"))
+            {
+                string inputXmlFile = Console.ReadLine();
+                string outputXmlFile = Console.ReadLine();
+                program.SerializeOutputXML(inputXmlFile, outputXmlFile);
+                Console.WriteLine("Сериализованный объект ответа получен в файле " + outputXmlFile);
+            }
+        }
+    }
+}
